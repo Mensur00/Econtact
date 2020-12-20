36a767de932def048b7a2f99f68022a7d7e999a8
Mensur Sejdinovic <mensursejdinovic@gmail.com>
2020-12-20 11:56:44 +0100

    Creating and connecting database

--- Econtact/Econtact.Designer.cs
+++ Econtact/Econtact.Designer.cs
@@ -60,7 +60,7 @@ namespace Econtact
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.Green;
-            this.label1.Location = new System.Drawing.Point(331, 9);
+            this.label1.Location = new System.Drawing.Point(331, 4);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(238, 55);
             this.label1.TabIndex = 0;
